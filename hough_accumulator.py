4c39092cc1d72c78b95a4c4960863de80f40212b
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-01-09 13:12:33 +0100

    Calculating whole range for accumulator to be able to mirror etc.

--- hough_accumulator.py
+++ hough_accumulator.py
@@ -5,8 +5,20 @@ import hough
 class HoughAccumulator:
     def __init__(self, img: np.ndarray):
         self.acc = hough.houghaccum(img)
+        self.rho_per_col = 1
+        self.theta_per_col = 1
         self.rows, self.cols = self.acc.shape
         self.rho_offset = int((self.cols - 1) / 2)
+        self.rows *= 2
+        mirror = self.acc.copy()
+        mirror = np.hstack((np.fliplr(mirror[:, -self.rho_offset:]), mirror[:, self.rho_offset:self.rho_offset+1], np.fliplr(mirror[:, :self.rho_offset])))
+        self.acc = np.vstack((self.acc, mirror))
+
+    def col_to_rho(self, col: int) -> float:
+        return ((col % self.cols) - self.rho_offset) * self.rho_per_col
+
+    def row_to_theta(self, row: int) -> float:
+        return (row * self.theta_per_col) % 360
 
     def theta_to_row(self, theta: float) -> int:
         return int(round(theta)) % self.rows
--- test_hough.py
+++ test_hough.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import pytest
 from pathlib import Path
+import random
 import hough
 from hough_accumulator import HoughAccumulator
 
@@ -33,3 +34,25 @@ def test_houghaccumulator():
     img[:, 5] = 1
     acc = HoughAccumulator(img)
     assert acc[0, 5] == 10
+
+
+def test_houghaccumulator_mirroring():
+    img = np.zeros((10, 10), dtype=np.uint8)
+    img[:, 5] = 1
+    acc = HoughAccumulator(img)
+    for i in range(100):
+        angle = random.randrange(0, 180, 1)
+        rho = random.randrange(0, int(acc.col_to_rho(-1)), 1)
+        assert acc[angle, rho] == acc[angle+180, -rho], f"Mirroring {angle},{rho} == {angle+180},{-rho}"
+    assert acc[180, -5] == 10
+
+
+def test_houghaccumulator_indexing():
+    img = np.zeros((10, 10), dtype=np.uint8)
+    img[:, 5] = 1
+    acc = HoughAccumulator(img)
+    assert acc.rho_offset == 20
+    assert acc.col_to_rho(0) == -20
+    assert acc.col_to_rho(-1) == 20
+    assert acc.row_to_theta(0) == 0
+    assert acc.row_to_theta(-1) == 359
